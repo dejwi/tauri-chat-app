4ec94e091afba997c7d7d51701743da133b30f58
dejwi <misti.krasula@gmail.com>
2023-05-09 20:23:37 +0200

    fix logging

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// #[macro_use]
+// extern crate log;
 
-use tauri_chat_app::client::{__cmd__test_client_connect, test_client_connect};
+use tauri_chat_app::client::{__cmd__client_connect, client_connect};
 use tauri_chat_app::host::{__cmd__host_server, host_server};
+use tauri_plugin_log::LogTarget;
 
 fn main() {
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_log::Builder::default()
+                .targets([LogTarget::Stdout])
+                .filter(|metadata| metadata.target().contains("tauri_chat_app"))
+                .build(),
+        )
         .invoke_handler(tauri::generate_handler![
             host_server,
             test_cmd,
-            test_client_connect
+            client_connect
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
